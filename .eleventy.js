2029bdf89d7686ea1a5c074d830aff5c02726407
averymc21 <125823066+averymc21@users.noreply.github.com>
2024-03-04 19:09:39 -0500

    added alpine.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,3 +3,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
+
+// _data/randomName.js
+const names = ['Olivia', 'Noah', 'Emma', 'Liam', 'Amelia', 'Oliver'];
+
+module.exports = function getRandomName() {
+    return names[Math.floor(Math.random() * names.length)];
+};
