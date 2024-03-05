5210ccf4eacaaa7308e58a1b03918c7da2d17e26
averymc21 <125823066+averymc21@users.noreply.github.com>
2024-03-04 22:43:17 -0500

    final edit

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,3 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
-
-// _data/randomName.js
-const names = ['Olivia', 'Noah', 'Emma', 'Liam', 'Amelia', 'Oliver'];
-
-module.exports = function getRandomName() {
-    return names[Math.floor(Math.random() * names.length)];
-};
